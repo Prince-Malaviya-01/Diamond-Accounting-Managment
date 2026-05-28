156a4136f5852f9467884f38ec6b8553c3092291
Prince Malaviya <princemalaviya01@gmail.com>
2026-05-28 10:43:21 +0530

    Expand weight input to 68px and set table cell buttons to inline-block with 135px max-width to fix text cutting

--- frontend/dist/assets/user-plus-BR55UyA0.js
+++ frontend/dist/assets/user-plus-BR55UyA0.js
@@ -1,4 +1,4 @@
-import{c as e}from"./api-DXMosPRu.js";/**
+import{c as e}from"./api-B7Hf7Oje.js";/**
  * @license lucide-react v0.577.0 - ISC
  *
  * This source code is licensed under the ISC license.
