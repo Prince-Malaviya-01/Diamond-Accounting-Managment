ddbb9bb46e45e4cb57d9719b21ae80d311ffa991
Prince Malaviya <princemalaviya01@gmail.com>
2026-05-28 15:48:04 +0530

    fix: temporary roll back to simplified landing page to isolate the silent rendering issues

--- frontend/dist/assets/api-DoFRbHf6.js
+++ frontend/dist/assets/api-DoFRbHf6.js
@@ -1,4 +1,4 @@
-import{c as bt}from"./index-BVOuNduv.js";/**
+import{c as bt}from"./index-8ljvVzF4.js";/**
  * @license lucide-react v0.577.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- frontend/dist/assets/eye-off-DrsNZj7k.js
+++ frontend/dist/assets/eye-off-DrsNZj7k.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BVOuNduv.js";/**
+import{c as e}from"./index-8ljvVzF4.js";/**
  * @license lucide-react v0.577.0 - ISC
  *
  * This source code is licensed under the ISC license.
