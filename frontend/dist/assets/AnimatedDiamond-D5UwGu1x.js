616237e31c0b6304325fe74b61d5414eb490079d
Prince Malaviya <princemalaviya01@gmail.com>
2026-06-01 17:39:07 +0530

    Increase responsive font size of landing hero title to make it larger and bolder on mobile and tablet devices

--- frontend/dist/assets/AnimatedDiamond-D5UwGu1x.js
+++ frontend/dist/assets/AnimatedDiamond-D5UwGu1x.js
@@ -1,4 +1,4 @@
-import{r as an,j as on}from"./index-DmNugDoi.js";/**
+import{r as an,j as on}from"./index-IeX3fwqL.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
